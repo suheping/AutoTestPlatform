651654fdbc0b17ba9438dfd7acfce6cd100891c9
shp@choicesoft.com.cn <suheping123>
2019-05-22 13:49:24 +0800

    添加对re字段是否为严禁json的异常捕获

--- util/regFindString.py
+++ util/regFindString.py
@@ -5,6 +5,10 @@
 # desc: 通过正则取值,传入多个正则的键值对，返回多个键值对
 
 import re,json
+import json.decoder
+from util.logUtil import Log
+
+logger = Log('regFindString')
 
 class regFindString:
     def __init__(self,str,regs):
@@ -13,25 +17,37 @@ class regFindString:
 
 
     def find(self):
-        reg = json.loads(self.regs)
         res = {}
-        for i in reg.keys():
-            r = re.findall(reg[i], self.str)
-            if r:
-                res[i] = r[0]
-            else:
-                res[i] = ''
-        return res
+        ''' 20190522修改，捕获string转字典异常，并打印错误日志'''
+        try:
+            # string转为字典格式
+            reg = json.loads(self.regs)
+            for i in reg.keys():
+                r = re.findall(reg[i], self.str)
+                if r:
+                    res[i] = r[0]
+                else:
+                    res[i] = ''
+        except json.decoder.JSONDecodeError as e:
+            # print("re字段不是标准json，请确认！,报错信息如下：",e)
+            logger.error("re字段不是标准json，请确认！,报错信息如下：\n %s" % e)
+        finally:
+            return res
 
 
 if __name__ == "__main__":
 
     tmp = {}
 
-    s1 = "var tableKey = '201810207131C9157F2D4FE3A59C111'; // 桌台码  " \
-         "var orderKey = '7131C9157F2D4FE3A59C_o29U5wq7XF-OutcJRojtmO2exjhY_orders';"
-
-    js = "{\"tablekey\":\"tableKey = '(.+?)';\",\"orderKey\":\"orderKey = '(.+?)';\"}"
+    s1 = '{"accessToken":"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiIl0sInVzZXJfbmFtZSI6IntcIm1vYmlsZVwiOlwiMTgwMDU0MTAzNTBcIixcImFpZFwiOlwiNDdlMzYyNjZlNWFhNDc3ZThkYjlmZGM2ZjEyMTdjZDBcIixcInVpZFwiOlwiLTFcIixcInRpZFwiOlwiNWFiZjAwMTc5NmQxOTAwMDBkMDdhNTlhXCIsXCJzaWRcIjpcIjVlODJiYTNjLWM5ODItNDUxOS04NzFkLWIxZmNhN2I3YzFhMlwiLFwiYXRva2VuXCI6XCI0OWJhOWM2NjkyNzU0ZWZlYWJlZThlN2M3M2EzNmFkMVwifSIsInNjb3BlIjpbInNjb3BlIl0sImV4cCI6MTU1ODQ0NDk4NCwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJqdGkiOiI0OTJhYzIyNi00OWFmLTQyYzAtYTQyNi1kNjE2OGQyYTdhNWMiLCJjbGllbnRfaWQiOiJCcm93c2VyIn0.U07Cc9IkaSga-4si-nIrbo7mOCx2f_oNv4_Sdw8FGT1spxle6NmKHjZtT2a4ZI8IfHvL3qYxq9L7_7HCJbsHQVOMlSbHVkUzI__KYUgB72lANxGffRedq0CG5LXC1Ua5ArNXCBWJ6cfgXn1f5JH-WpGDmStBg_6MSRlmpToT6go","refreshToken":"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiIl0sInVzZXJfbmFtZSI6IntcIm1vYmlsZVwiOlwiMTgwMDU0MTAzNTBcIixcImFpZFwiOlwiNDdlMzYyNjZlNWFhNDc3ZThkYjlmZGM2ZjEyMTdjZDBcIixcInVpZFwiOlwiLTFcIixcInRpZFwiOlwiNWFiZjAwMTc5NmQxOTAwMDBkMDdhNTlhXCIsXCJzaWRcIjpcIjVlODJiYTNjLWM5ODItNDUxOS04NzFkLWIxZmNhN2I3YzFhMlwiLFwiYXRva2VuXCI6XCI0OWJhOWM2NjkyNzU0ZWZlYWJlZThlN2M3M2EzNmFkMVwifSIsInNjb3BlIjpbInNjb3BlIl0sImF0aSI6IjQ5MmFjMjI2LTQ5YWYtNDJjMC1hNDI2LWQ2MTY4ZDJhN2E1YyIsImV4cCI6MTU1ODQ1MDM4NCwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJqdGkiOiJkMjIwM2YwZC1kYWRkLTQ4NTItYWFjNC00N2U2ODVkNDZlMDYiLCJjbGllbnRfaWQiOiJCcm93c2VyIn0.Wm3aSeovA32vtiCxAZm98z0XgiWbjQV7NTEMymw07MZ3XxhJ3xZJ7QHl_aTfaqTEiXteQnZUzrVwkW8WTQbRUoBsPVk3YaCFP6FPt0HO0vD9rdT5da-mX2q-QQv2-0_Lqegl8S2AwK2ka7bV7C8SxxX-UF24e7xv1hZEOHJRUVQ._f344b80e4120446d9835d8725ac9b8b3_."}'
+    dic_a = json.loads(s1)
+    print(dic_a["accessToken"])
+    # js = "{\"accessToken\": \"a\"}"
+    # js = "{'accessToken': 'accessToken\": \"(.+?)\"'}"
+    # js = "{"accessToken": "accessToken\": \"(.+?)\","}"
+    js = '{"accessToken": "accessToken\\\": \\\"(.+?)\\\",","refreshToken": "refreshToken\\\": \\\"(.+?)\\\""}'
+    # dic_b = json.loads(js1)
+    # print(dic_b["accessToken"])
 
     params = regFindString(s1,js).find()
     print(params)
@@ -43,6 +59,8 @@ if __name__ == "__main__":
          "var orderid = '7131C9157F2D4FE3A59C_o29U5wq7XF-OutcJRojtmO2exjhY_orders';"
 
     js2 = "{\"tablekey\":\"tableKey = '(.+?)';\",\"orderid\":\"orderid = '(.+?)';\"}"
+
+    # js3 = "{"tablekey":"tableKey = '(.+?)';","orderid":"orderid = '(.+?)';"}"
 
     params2 = regFindString(s2,js2).find()
 
